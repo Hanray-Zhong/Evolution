4fb9b060f3383a8c3c77cb9b0a79e8e7aabc6849
kaneto <646374316@qq.com>
2019-07-16 18:27:20 +0800

    fea : 7.16
    - 完成摄象机视角控制
    - 救险

--- Assets/Scripts/Camera/MoveCamera.cs
+++ Assets/Scripts/Camera/MoveCamera.cs
@@ -1,17 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using KanetoTools;
 
 public class MoveCamera : MonoBehaviour
 {
-    public float Speed;
-    private Vector2 lastMousePos = Vector2.zero;
-    private Vector2 velocity;
+    
+    private Vector2 MousePosition;
+    private Vector2 relativePos;
+
+    public BoxCollider2D Edge;
+    private float edgeHeight;
+    private float edgeWidth;
+    private float cameraHeight;
+    private float cameraWidth;
+    
+    private void Start() {
+        Init();
+    }
 
     void Update() {
-        Vector2 currentMousePos = Input.mousePosition;
-		velocity = (currentMousePos - lastMousePos).normalized;
-        gameObject.GetComponent<Transform>().Translate(velocity * Speed * Time.deltaTime, Space.World);
-        lastMousePos = currentMousePos;
+        // Debug.Log(Input.mousePosition);
+        MousePosition = new Vector2(Mathf.Clamp(Input.mousePosition.x, 0, Screen.width), Mathf.Clamp(Input.mousePosition.y, 0, Screen.height)) - new Vector2(Screen.width / 2, Screen.height / 2);
+        relativePos = MousePosition * ((edgeWidth - cameraWidth) / Screen.width);
+        Debug.Log(cameraWidth);
+        gameObject.transform.SetPositionAndRotation(new Vector3(relativePos.x, relativePos.y, gameObject.transform.position.z), Quaternion.identity);
+    }
+
+
+    void Init() {
+        edgeHeight = Edge.bounds.max.y - Edge.bounds.min.y;
+        edgeWidth = Edge.bounds.max.x - Edge.bounds.min.x;
+        Debug.Log(edgeWidth);
+        Vector3 Screen_max = new Vector3(Screen.width, Screen.height, 0);
+        cameraHeight = 2 * gameObject.GetComponent<Camera>().ScreenToWorldPoint(Screen_max).y;
+        cameraWidth = 2 * gameObject.GetComponent<Camera>().ScreenToWorldPoint(Screen_max).x;
+    }
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = new Color(1, 0, 0, 1);
+        Vector3 a = new Vector3((cameraWidth) / 2, (cameraHeight) / 2, 0);
+        Vector3 b = new Vector3((cameraWidth) / 2, -(cameraHeight) / 2, 0);
+        Vector3 c = new Vector3(-(cameraWidth) / 2, -(cameraHeight) / 2, 0);
+        Vector3 d = new Vector3(-(cameraWidth) / 2, (cameraHeight) / 2, 0);
+        
+        Gizmos.DrawLine(a, b);
+        Gizmos.DrawLine(a, d);
+        Gizmos.DrawLine(c, b);
+        Gizmos.DrawLine(c, d);
+        
     }
 }
